7ff049fcc2cb617d5781ccc526b7e40da0d407f5
keremkiyak <110934008+keremkiyak@users.noreply.github.com>
2023-02-04 14:58:08 +0300

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var isim = "kerem" //burda olduğu gibi tip tanımlaması yapmazsam
+//derleyici otomatikman bu değişeknin tipini anlar yani tip belirtmeye gerek yok
+let yas = 22
+
+
+
+var buYil : Int = 2023//let sabit  oluşturmak için kullanılır
+// let ile belirtilen değişmez ama var ile belirtilen değiştirilebilir.
+let gecenYil : Int = 2022
+
+
+//Double 64 bit sayi tutabilir
+//Float 32 bit sayi tutabilir
+//birde Book tipi vardir onun karşılığı da ya true yada false dir.
