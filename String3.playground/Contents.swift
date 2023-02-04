b05ab782fe2bdb735b1ca702deb86ff35b463de9
keremkiyak <110934008+keremkiyak@users.noreply.github.com>
2023-02-04 15:11:49 +0300

    Add files via upload

---
+++ String3.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var name = "ali"
+name.append("!")
+print(name)//append sonuna ekleme yapar
+
+var name2 = ""//bos bir string olusturma
+print(name.count)//count ile string in içerisindeki karakter sayisini alabiliriz.
+print(name[name.startIndex])//buda bize string in ilk karakterini verir.
